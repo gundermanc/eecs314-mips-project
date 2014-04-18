f9a4ddcbeb3f46ded6eb32b05491f9aff2718cf5
Christian Gunderman <gundermanc@gmail.com>
2014-04-17 20:46:20 -0400

    Created routine macros
    Signed-off-by: Christian Gunderman <gundermanc@gmail.com>

--- gundermanc-macros.asm
+++ gundermanc-macros.asm
@@ -1,7 +1,8 @@
 # Useful macros for printing to console, etc.
 # By: Christian Gunderman
 
-# Put at top of your source file to include this file: .include "gundermanc-macros.asm"
+# Put at top of your source file to include this file:
+#   .include "gundermanc-macros.asm"
 
 
 
@@ -18,6 +19,29 @@
 	li   $v0, 1
 	syscall
 .end_macro
+
+# pushes a return value to the stack
+.macro	push_return_value
+	addi	$sp, $sp, -4		# move stack pointer
+	sw 	$ra, ($sp)		# store return address
+.end_macro
+
+# pop return value from stack
+.macro pop_return_value
+	lw	$ra, ($sp)		# load return address to $ra
+	addi	$sp, $sp, 4		# move stack pointer
+.end_macro
+
+# defines a routine
+.macro begin_routine (%name)
+	%name:
+	push_return_value
+.end_macro
+
+.macro end_routine
+	pop_return_value
+	jr	$ra
+.end_macro 
 
 # ends the program 
 .macro	exit
